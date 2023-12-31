8bb4eca9bb48aef85d2e0e105f36d6017e40a805
VidasBui <107630990+VidasBui@users.noreply.github.com>
2023-12-31 14:57:03 +0200

    feat(game grid): place ships

--- server/src/models/GameGrid.ts
+++ server/src/models/GameGrid.ts
@@ -1,3 +1,5 @@
+import _ from "lodash";
+import { v4 as uuid } from "uuid";
 import Ship from "./Ship";
 
 type Tile = {
@@ -10,9 +12,9 @@ export type TileKey = { x: number; y: number };
 class GameGrid {
   private _grid: Record<string, any> = {};
 
-  constructor() {
-    for (let i = 0; i < 10; i++) {
-      for (let j = 0; j < 10; j++) {
+  constructor(public size = 10) {
+    for (let i = 0; i < size; i++) {
+      for (let j = 0; j < size; j++) {
         const key: TileKey = { x: i, y: j };
         const defaultTile: Tile = {
           ship: undefined,
@@ -21,6 +23,7 @@ class GameGrid {
         this.setTile(key, defaultTile);
       }
     }
+    this.placeShips();
   }
 
   private getTile(key: TileKey): Tile | undefined {
@@ -32,6 +35,96 @@ class GameGrid {
     const keyString = JSON.stringify(key);
     this._grid[keyString] = value;
   }
+
+  private placeShips() {
+    for (const shipLength of getDefaultShipLengths()) {
+      let placed = false;
+      while (!placed) {
+        const startX = Math.floor(Math.random() * (this.size - shipLength + 1));
+        const startY = Math.floor(Math.random() * (this.size - shipLength + 1));
+        const isHorizontal = Math.random() < 0.5;
+        const shipId: string = uuid();
+        if (
+          this.canPlaceShip(shipLength, startX, startY, isHorizontal, shipId)
+        ) {
+          this.placeShip(shipLength, startX, startY, isHorizontal, shipId);
+          placed = true;
+        }
+      }
+    }
+  }
+
+  private placeShip(
+    shipLength: number,
+    startX: number,
+    startY: number,
+    isHorizontal: boolean,
+    shipId: string
+  ) {
+    let ship = new Ship(shipId, []);
+    for (let i = 0; i < shipLength; i++) {
+      const x = isHorizontal ? startX + i : startX;
+      const y = isHorizontal ? startY : startY + i;
+      const key: TileKey = { x: x, y: y };
+      ship.occupiedTiles.push(key);
+
+      const tile: Tile = {
+        ship,
+        wasShot: false,
+      };
+
+      this.setTile(key, tile);
+    }
+    return ship;
+  }
+
+  private canPlaceShip(
+    shipLength: number,
+    startX: number,
+    startY: number,
+    isHorizontal: boolean,
+    shipId: string
+  ): boolean {
+    for (let i = 0; i < shipLength; i++) {
+      let x = isHorizontal ? startX + i : startX;
+      let y = isHorizontal ? startY : startY + i;
+      if (this.isOccupied(x, y, shipId)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  private isOccupied(x: number, y: number, shipIdToIgnore: string): boolean {
+    for (let xi = -1; xi <= 1; xi++) {
+      for (let yi = -1; yi <= 1; yi++) {
+        const neighborX = x + xi;
+        const neighborY = y + yi;
+        const key: TileKey = { x: neighborX, y: neighborY };
+
+        const tile = this.getTile(key);
+
+        if (
+          tile &&
+          tile.ship !== undefined &&
+          tile.ship.shipId !== shipIdToIgnore
+        ) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
 }
+
+const getDefaultShipLengths = () =>
+  _.concat(
+    _.times(1, () => 5),
+    _.times(1, () => 4),
+    _.times(2, () => 3),
+    _.times(3, () => 2),
+    _.times(3, () => 1)
+  );
 
 export default GameGrid;

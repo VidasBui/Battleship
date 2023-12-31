7be11c99f1a0e089a097c358493c7c096dc906dd
VidasBui <107630990+VidasBui@users.noreply.github.com>
2023-12-31 20:10:07 +0200

    feat(controller): add shoot controller

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import cors from "cors";
 import express from "express";
 import { v4 as uuid } from "uuid";
 import GameStartController from "./controllers/GameStartController";
+import shootController from "./controllers/shootController";
 
 export const secretKey = process.env.JWT_SECRET_KEY || uuid();
 const port = 8000;
@@ -19,6 +20,7 @@ app.use(
 app.use(express.json());
 
 app.get("/api/startGame", GameStartController);
+app.post("/api/shoot", shootController);
 
 app.get("/api", (req, res) => {
   res.send("Battleship game");

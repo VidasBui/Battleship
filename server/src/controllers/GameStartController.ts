d17c115b05cb6f97c947c1d6f31daf7a189d958a
VidasBui <107630990+VidasBui@users.noreply.github.com>
2023-12-31 14:58:14 +0200

    feat(game start controller): create new game

--- server/src/controllers/GameStartController.ts
+++ server/src/controllers/GameStartController.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express";
 import jwt from "jsonwebtoken";
 import { v4 as uuid } from "uuid";
+import Game from "../models/Game";
+import gameRepository from "../repositories/GameRepository";
 
 const secretKey = process.env.JWT_SECRET_KEY || uuid();
 
@@ -10,6 +12,9 @@ const GameStartController = (req: Request, res: Response): void => {
   const token: string = jwt.sign({ gameId }, secretKey, {
     expiresIn: "4h",
   });
+
+  var game = new Game(gameId);
+  gameRepository.add(game);
 
   res.json(token);
 };

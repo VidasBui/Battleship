fa0ae415948ae0a79727bc5d9d902c176b9324d5
VidasBui <107630990+VidasBui@users.noreply.github.com>
2024-01-03 16:04:33 +0200

    feat(component): add GridHeader component

---
+++ client/src/components/GridHeader.tsx
@@ -0,0 +1,27 @@
+import { Stack } from "@mui/material";
+import Cell from "./Cell";
+
+type Props = {
+  length: number;
+};
+
+const GridHeader = ({ length }: Props) => {
+  const generateAlphabetCharacter = (index: number): string => {
+    const charCodeA = "A".charCodeAt(0);
+    return String.fromCharCode(charCodeA + index);
+  };
+
+  return (
+    <Stack direction="row" spacing={0.5} justifyContent="center">
+      <Cell key={`header`} />
+      {Array.from({ length: length }, (_, colIndex) => (
+        <Cell
+          key={`header-${colIndex}`}
+          text={generateAlphabetCharacter(colIndex)}
+        />
+      ))}
+    </Stack>
+  );
+};
+
+export default GridHeader;

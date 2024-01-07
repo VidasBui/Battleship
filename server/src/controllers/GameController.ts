2372e5bbfb51dc1e572fd6e8434897f37d8b7109
VidasBui <107630990+VidasBui@users.noreply.github.com>
2024-01-07 22:30:55 +0200

    refactor(GameController): use AuthService on controllers

--- server/src/controllers/GameController.ts
+++ server/src/controllers/GameController.ts
@@ -1,15 +1,16 @@
 import { Request, Response } from "express";
 import Joi from "joi";
-import jwt from "jsonwebtoken";
-import { secretKey } from "../index";
 import { TileKey } from "../models/GameGrid";
 import gameService from "../services/GameService";
+import authService from "../services/AuthService";
 
 class GameController {
   async startGame(req: Request, res: Response) {
     try {
-      const { token, grid, gridSize, remainingShips, remainingShots } =
+      const { gameId, grid, gridSize, remainingShips, remainingShots } =
         gameService.startNewGame();
+
+      const token = authService.getToken(gameId);
 
       res.header("Authorization", `Bearer ${token}`);
       res.status(200).json({ grid, remainingShots, gridSize, remainingShips });
@@ -30,25 +31,16 @@ class GameController {
         return res.status(400).json({ message: "Bad request" });
       }
 
-      const token = req.header("Authorization")?.split(" ")[1];
+      const { status, errMessage, gameId } = authService.validateToken(req);
 
-      if (!token) {
-        return res.status(401).json({ message: "Unauthorized" });
-      }
+      if (!gameId) return res.status(status).json({ message: `${errMessage}` });
 
-      jwt.verify(token, secretKey, (err: any, decoded: any) => {
-        if (err) {
-          return res.status(403).json({ message: "Forbidden" });
-        }
+      const coordinates: TileKey = req.body;
+      const r = gameService.handleShot(gameId, coordinates);
 
-        const coordinates: TileKey = req.body;
-        const gameId: string = decoded.gameId;
-        const r = gameService.handleShot(gameId, coordinates);
+      if (r.error) return res.status(400).json({ message: r.error });
 
-        if (r.error) return res.status(400).json({ message: r.error });
-
-        return res.status(200).json({ ...r });
-      });
+      return res.status(200).json({ ...r });
     } catch (e: any) {
       return res.status(500).json({ errorMessage: e.message });
     }
